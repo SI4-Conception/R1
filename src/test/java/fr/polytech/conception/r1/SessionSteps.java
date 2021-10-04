02a91ac1b30d68df57b580ef159a3bdd7c7f2026
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-10-04 22:16:43 +0200

    create session unit test

--- src/test/java/fr/polytech/conception/r1/SessionSteps.java
+++ src/test/java/fr/polytech/conception/r1/SessionSteps.java
@@ -1,10 +1,15 @@
 package fr.polytech.conception.r1;
 
+import org.junit.Assert;
+
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class SessionSteps
 {
+    Session session;
+
     @Given("valid datas for the session")
     public void validDatasForTheSession()
     {
@@ -13,5 +18,12 @@ public class SessionSteps
     @When("I create the session")
     public void iCreateTheSession()
     {
+        session = new Session();
+    }
+
+    @Then("It should have been a success")
+    public void itShouldHaveBeenASuccess()
+    {
+        Assert.assertNotNull(session);
     }
 }
--- src/test/java/fr/polytech/conception/r1/test-service.feature
+++ src/test/java/fr/polytech/conception/r1/test-service.feature
@@ -3,4 +3,4 @@ Feature: Features of my service
   Scenario: Calling my service
     Given my service exists
     When I call my service
-    Then it should have been a success
+    Then It should have been a success
