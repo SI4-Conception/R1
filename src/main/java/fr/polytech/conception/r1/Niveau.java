0be85eaea07e2e15bce951030fe2df5fa59cebb5
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-10-06 13:45:00 +0200

    Add Session class

---
+++ src/main/java/fr/polytech/conception/r1/Niveau.java
@@ -0,0 +1,9 @@
+package fr.polytech.conception.r1;
+
+public enum Niveau
+{
+    DEBUTANT,
+    INTERMEDIAIRE,
+    AVANCE,
+    EXPERT
+}
--- src/main/java/fr/polytech/conception/r1/Session.java
+++ src/main/java/fr/polytech/conception/r1/Session.java
@@ -1,5 +1,20 @@
 package fr.polytech.conception.r1;
 
+import java.time.LocalDateTime;
+
 public class Session
 {
+    private LocalDateTime debut;
+    private LocalDateTime fin;
+    private LocalDateTime dateLimiteInscription;
+    private String adresse;
+    private boolean reserveAuxAmis;
+    private int minParticipants;
+    private int maxParticipants;
+    private Niveau difficulte;
+    private boolean estAnnulee;
+
+    public Session() {
+        //
+    }
 }
