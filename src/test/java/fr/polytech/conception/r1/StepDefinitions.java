2f70f1f61840596f6577ee6cdac167ca40e7dfa8
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-10-18 11:17:06 +0200

    Remove redundant imports

--- src/test/java/fr/polytech/conception/r1/StepDefinitions.java
+++ src/test/java/fr/polytech/conception/r1/StepDefinitions.java
@@ -1,12 +1,6 @@
 package fr.polytech.conception.r1;
 
-import java.util.Locale;
-
-import io.cucumber.core.api.TypeRegistry;
-import io.cucumber.core.api.TypeRegistryConfigurer;
 import io.cucumber.java.ParameterType;
-
-import static java.util.Locale.ENGLISH;
 
 public class StepDefinitions
 {
